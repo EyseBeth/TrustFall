b27a0ce46c859c0f042700dd86c0476baf56e4d2
Lisa <lisatgroen@hotmail.com>
2017-09-28 17:29:58 +0200

    buttons + door, door disappearable

---
+++ TrustFall/Assets/HierDoorAway.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HierDoorAway : MonoBehaviour {
+
+    public GameObject hierdoor;
+
+    void OnMouseDown()
+    {
+        hierdoor.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+}
